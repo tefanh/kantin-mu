db03a36df77ae4b4d6ae9eedafe8bedb58cfbca2
Tefan Haetami <tefanhaetami87@gmail.com>
2022-11-29 21:30:31 +0700

    feat: slicing reset password page

---
+++ src/pages/forgot-password/Index.vue
@@ -0,0 +1,56 @@
+<script lang="ts">
+  export default {
+    data: () => ({
+      form: {
+        isValid: true,
+        email: '',
+      },
+    }),
+  }
+</script>
+<template>
+  <div>
+    <Nav actionTitle="Lewati" />
+    <v-container>
+      <v-form :modelValue="form.isValid">
+        <h6 class="text-h6 mb-2">Lupa Password</h6>
+        <p class="mb-6 text-caption">
+          Masukkan alamat email Anda untuk meminta reset kata sandi.
+        </p>
+
+        <div>
+          <p class="text-body-2 mb-1">Email</p>
+          <v-text-field
+            type="email"
+            placeholder="Masukan email"
+            variant="outlined"
+            density="comfortable" />
+        </div>
+
+        <div class="login__actions fullwidth">
+          <v-container>
+            <v-btn
+              type="submit"
+              color="primary"
+              block
+              flat
+              rounded
+              class="font-weight-bold text-capitalize mb-4"
+              :disabled="!form.isValid">
+              Reset Password
+            </v-btn>
+          </v-container>
+        </div>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .login {
+    &__actions {
+      position: fixed;
+      bottom: 0px;
+      left: 0;
+    }
+  }
+</style>
--- src/router.config.ts
+++ src/router.config.ts
@@ -3,11 +3,15 @@ import * as VueRouter from 'vue-router'
 import Home from './pages/Index.vue'
 import Intro from './pages/Intro.vue'
 import Login from './pages/login/Index.vue'
+import Register from './pages/register/Index.vue'
+import ForgotPassword from './pages/forgot-password/Index.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/intro', component: Intro },
   { path: '/login', component: Login },
+  { path: '/register', component: Register },
+  { path: '/forgot-password', component: ForgotPassword },
 ]
 
 export const router = VueRouter.createRouter({
